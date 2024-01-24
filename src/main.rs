fc6618f9670c0e90e6178a5b8a1580710d427ddf
simplysabir <simplysabir@gmail.com>
2024-01-24 17:21:05 +0530

    basic impl

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,12 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
 
 
